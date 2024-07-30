651bfc25cf9b80d69712e3dc73be7fe3513808af
Adil Gahalta <adilgahalta1@gmail.com>
2024-07-30 23:15:04 +0700

    push

---
+++ KFC/kfc-website/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  return (
+    <div>
+      <div>
+        <img src="https://files.kfcku.com/uploads/media/logo-red.png" alt="" />
+      </div>
+    </div>
+  );
+}
